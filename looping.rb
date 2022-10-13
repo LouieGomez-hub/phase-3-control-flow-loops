a35ca839857886cef3577860ac24b072a44c1106
louie_gomez <kinggomez027.lg@gmail.com>
2022-10-13 16:33:50 -0400

    commit tree 31db8c6495b485903a505b93b3692b158534b19d

--- looping.rb
+++ looping.rb
@@ -27,5 +27,9 @@ def fizzbuzz_printer
 end
 
 def reverse_string(str)
-  # your code here
+  reversed_str = ""
+  str.length.times do |i|
+    reversed_str = str[i] + reversed_str
+  end
+  reversed_str
 end
